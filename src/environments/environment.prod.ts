6282c91cbe90a5bef4673b19d38f9b4e37f66d34
premprakash yadav <84430639+premprakash8080@users.noreply.github.com>
2025-12-23 17:21:48 +0530

    updated the url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.example.com/api',
+  apiUrl: 'http://77.37.62.160:3012/api',
   autoSaveDelay: 30000 // 30 seconds in milliseconds
 };
